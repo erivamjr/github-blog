5527c2c34673931ca01a07e20bc662deb09a9149
José Erivam <erivamdev@gmail.com>
2024-05-15 18:23:16 -0300

    :recycle: refactor: remove css code

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,21 +6,14 @@ export const HeaderContainer = styled.header`
   padding: 10px;
 `
 export const HeaderContent = styled.div`
-  width: 100%;
   max-width: 1440px;
   margin: 0 auto;
   height: 18.5rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  overflow: hidden;
 
-  img:nth-child(1) {
-  }
   img:nth-child(2) {
     margin-bottom: 8rem;
-  }
-
-  img:nth-child(3) {
   }
 `
